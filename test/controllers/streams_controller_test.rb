36dee53fe09f84b380ffd736c2fe3f3cdff78ae2
Yong Bakos <ybakos@humanoriented.com>
2023-08-23 11:59:03 -0700

    11 Test controller create, update, destroy redirects

---
+++ test/controllers/streams_controller_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class TurboClone::StreamsControllerTest < ActionDispatch::IntegrationTest
+
+  test 'create with respond to' do
+    post articles_path, params: { article: { content: 'fake' } }
+    assert_redirected_to articles_path
+  end
+
+  test 'update with respond to' do
+    article = Article.create!(content: 'fake')
+    patch article_path(article), params: { article: { content: 'new content' } }
+    assert_redirected_to articles_path
+  end
+
+  test 'destroy with respond to' do
+    article = Article.create!(content: 'fake')
+    delete article_path(article)
+    assert_redirected_to articles_path
+  end
+
+end
