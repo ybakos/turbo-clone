d4bd409dfe1fe6ee5baeddc0ea3ead8c0d7fb15f
Yong Bakos <ybakos@humanoriented.com>
2023-08-24 13:23:41 -0700

    12 Test the template content generated by TagBuilder

--- test/controllers/streams_controller_test.rb
+++ test/controllers/streams_controller_test.rb
@@ -26,4 +26,16 @@ class TurboClone::StreamsControllerTest < ActionDispatch::IntegrationTest
     assert_turbo_stream action: 'remove', target: article
   end
 
+  test "show all turbo actions" do
+    article = Article.create!(content: 'Fake')
+    get article_path(article), as: :turbo_stream
+    assert_dom_equal <<~HTML, @response.body
+      <turbo-stream action="remove" target="#{dom_id(article)}"></turbo-stream>
+      <turbo-stream action="update" target="#{dom_id(article)}"><template>#{ApplicationController.render(article)}</template></turbo-stream>
+      <turbo-stream action="replace" target="#{dom_id(article)}"><template>Fake content</template></turbo-stream>
+      <turbo-stream action="prepend" target="articles"><template>#{ApplicationController.render(article)}</template></turbo-stream>
+      <turbo-stream action="prepend" target="articles"><template>#{ApplicationController.render(article)}</template></turbo-stream>
+    HTML
+  end
+
 end
