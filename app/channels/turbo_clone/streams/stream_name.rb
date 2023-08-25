89bd0efea6ff53ca0fa2d85362f7b8e9e1f31db8
Yong Bakos <ybakos@humanoriented.com>
2023-08-25 14:10:49 -0700

    15 Extract stream_name_from method into module

---
+++ app/channels/turbo_clone/streams/stream_name.rb
@@ -0,0 +1,15 @@
+module TurboClone
+  module Streams
+    module StreamName
+      def stream_name_from(streamables)
+        if streamables.is_a?(Array)
+          streamables.map { |streamable| stream_name_from(streamable) }.join(":")
+        else
+          streamables.then do |streamable|
+            streamable.respond_to?(:to_key) ? ActionView::RecordIdentifier.dom_id(streamable) : streamable
+          end
+        end
+      end
+    end
+  end
+end
--- app/channels/turbo_clone/streams_channel.rb
+++ app/channels/turbo_clone/streams_channel.rb
@@ -1,14 +1,6 @@
 module TurboClone
   class StreamsChannel < ActionCable::Channel::Base
-    def self.stream_name_from(streamables)
-      if streamables.is_a?(Array)
-        streamables.map { |streamable| stream_name_from(streamable) }.join(":")
-      else
-        streamables.then do |streamable|
-          streamable.respond_to?(:to_key) ? ActionView.RecordIdentifier.dom_id(streamable) : streamable
-        end
-      end
-    end
+    extend TurboClone::Streams::StreamName
 
     def subscribed
       stream_from params[:signed_stream_name]
