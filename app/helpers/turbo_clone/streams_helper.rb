b04c78cdd4f898f1c66047237a3998b97fb551b9
Yong Bakos <ybakos@humanoriented.com>
2023-08-25 13:59:17 -0700

    15 Define initial turbo_stream_from method

--- app/helpers/turbo_clone/streams_helper.rb
+++ app/helpers/turbo_clone/streams_helper.rb
@@ -3,5 +3,11 @@ module TurboClone
     def turbo_stream
       TurboClone::Streams::TagBuilder.new(self)
     end
+
+    def turbo_stream_from(*streamables, **attributes)
+      attributes[:channel] = "TurboClone::StreamsChannel"
+      attributes[:'signed-stream-name'] = TurboClone::StreamsChannel.stream_name_from(streamables)
+      tag.turbo_cable_stream_source(**attributes)
+    end
   end
 end
