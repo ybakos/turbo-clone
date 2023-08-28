5c551b2086435fa407623a2596a02b1913076787
Yong Bakos <ybakos@humanoriented.com>
2023-08-28 08:23:00 -0700

    18 Define broadcast_target_default

--- app/models/concerns/turbo_clone/broadcastable.rb
+++ app/models/concerns/turbo_clone/broadcastable.rb
@@ -1,6 +1,20 @@
 module TurboClone
   module Broadcastable
-    def broadcast_append_to
+    extend ActiveSupport::Concern
+
+    class_methods do
+      def broadcast_target_default
+        model_name.plural
+      end
+    end
+
+    def broadcast_append_to(*streamables, target: broadcast_target_default)
+    end
+
+    private
+
+    def broadcast_target_default
+      self.class.broadcast_target_default
     end
   end
 end
