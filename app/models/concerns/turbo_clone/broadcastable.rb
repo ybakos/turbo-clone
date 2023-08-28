d1ada7f22e8eaff10d6829bc3c0340dcf59199fa
Yong Bakos <ybakos@humanoriented.com>
2023-08-28 08:31:20 -0700

    18 Prepare broadcast_rendering_with_defaults method for generating rendering options

--- app/models/concerns/turbo_clone/broadcastable.rb
+++ app/models/concerns/turbo_clone/broadcastable.rb
@@ -8,10 +8,17 @@ module TurboClone
       end
     end
 
-    def broadcast_append_to(*streamables, target: broadcast_target_default)
+    def broadcast_append_to(*streamables, target: broadcast_target_default, **rendering)
     end
 
     private
+
+    def broadcast_rendering_with_defaults(rendering)
+      rendering.tap do |r|
+        r[:locals] = (r[:locals] || {}).reverse_merge!(model_name.element.to_sym => self)
+        r[:partial] ||= to_partial_path
+      end
+    end
 
     def broadcast_target_default
       self.class.broadcast_target_default
