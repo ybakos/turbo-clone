18308c15b225ce6d143d219bf0aea4acadda1fa3
Yong Bakos <ybakos@humanoriented.com>
2023-08-31 15:57:57 -0700

    23 Test the broadcast_later methods

--- test/channels/broadcastable_test.rb
+++ test/channels/broadcastable_test.rb
@@ -3,11 +3,21 @@ require 'test_helper'
 class TurboClone::BroadcastableTest < ActionCable::Channel::TestCase
 
   include TurboClone::ActionHelper
+  include ActiveJob::TestHelper
 
   test "#broadcast_append_to" do
     article = Article.create! content: "Fake"
     assert_broadcast_on "stream", turbo_stream_action_tag(:append, target: "articles", template: render(article)) do
       article.broadcast_append_to "stream"
+    end
+  end
+
+  test "#broadcast_append_later_to" do
+    article = Article.create! content: "Fake"
+    assert_broadcast_on "stream", turbo_stream_action_tag(:append, target: "articles", template: render(article)) do
+      perform_enqueued_jobs do
+        article.broadcast_append_later_to "stream"
+      end
     end
   end
 
@@ -18,10 +28,28 @@ class TurboClone::BroadcastableTest < ActionCable::Channel::TestCase
     end
   end
 
+  test "#broadcast_prepend_later_to" do
+    article = Article.create! content: "Fake"
+    assert_broadcast_on "stream", turbo_stream_action_tag(:prepend, target: 'fake', template: render(article)) do
+      perform_enqueued_jobs do
+        article.broadcast_prepend_later_to "stream", target: 'fake'
+      end
+    end
+  end
+
   test "#broadcast_replace_to" do
     article = Article.create! content: "Fake"
     assert_broadcast_on "stream", turbo_stream_action_tag(:replace, target: article, template: render(article)) do
       article.broadcast_replace_to "stream"
+    end
+  end
+
+  test "#broadcast_replace_later_to" do
+    article = Article.create! content: "Fake"
+    assert_broadcast_on "stream", turbo_stream_action_tag(:replace, target: article, template: render(article)) do
+      perform_enqueued_jobs do
+        article.broadcast_replace_later_to "stream"
+      end
     end
   end
 
