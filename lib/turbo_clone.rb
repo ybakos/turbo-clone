a3dc5ecf68c0e93ede2ad5d2707399ddeac0ab4c
Yong Bakos <ybakos@humanoriented.com>
2023-09-15 12:15:18 -0700

    26 Implement signed_stream_verifier_key and engine initializers

--- lib/turbo_clone.rb
+++ lib/turbo_clone.rb
@@ -2,5 +2,11 @@ require "turbo_clone/version"
 require "turbo_clone/engine"
 
 module TurboClone
-  # Your code goes here...
+  class << self
+    attr_writer :signed_stream_verifier_key
+
+    def signed_stream_verifier_key
+      @signed_stream_verifier_key or raise ArgumentError, "Turbo requires a signed_stream_verifier_key"
+    end
+  end
 end
